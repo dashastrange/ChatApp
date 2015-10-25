8b8d51b2b5ca5df260d08512b15d397d86be8355
artemka124 <avl123sim@mail.ru>
2015-10-25 20:43:14 +0200

    Create ClientMain.java
    U can test how client works

---
+++ src/ClientMain.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+public class ClientMain {
+
+	public static void main(String[] args) throws UnknownHostException {
+		 String YourName1="Client";
+	     int port1 = 28411;
+	     Messeger m = null;
+		 InetAddress addr = InetAddress.getByName("127.1.0.1");
+		 Caller c=new Caller(addr,port1,YourName1);
+         try {
+			 m = new Messeger(c.Connector());
+		} catch (IOException e) {
+			System.out.println("MAIN PROBLEM.");
+		}
+        m.Send("Hello bitch");
+	}
+
+}
